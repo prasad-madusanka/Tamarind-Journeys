1136f9f3ea2d2ad49f3599170f3d45455a4ad330
prasadMadusanka <prasad.mads@gmail.com>
2018-10-07 22:57:20 +0530

    Created required components, Working on Tours

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/common/navigation/navigation.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { HomeComponent } from './components/body/home/home.component';
+import { ToursComponent } from './components/body/tours/tours.component';
+import { ExcursionsComponent } from './components/body/excursions/excursions.component';
+import { EventsComponent } from './components/body/events/events.component';
+import { WildlifeComponent } from './components/body/wildlife/wildlife.component';
+import { GalleryComponent } from './components/body/gallery/gallery.component';
+import { CultureComponent } from './components/body/culture/culture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    HomeComponent,
+    ToursComponent,
+    ExcursionsComponent,
+    EventsComponent,
+    WildlifeComponent,
+    GalleryComponent,
+    CultureComponent
   ],
   imports: [
     BrowserModule
